f08c98cebdc16d415e2dafd55a0c1ac79dd51f6d
Andy <hinchliffe106@gmail.com>
2024-04-07 11:27:23 +0100

    basic logic

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+const bandTitles = document.getElementById('bandTitle')
+
+const but1 = document.getElementById('but1').addEventListener('click', () => {
+    bandTitles.innerText='band 1'
+})
+
+const but2 = document.getElementById('but2').addEventListener('click', () => {
+    bandTitles.innerText='band 2'
+})
+
+const but3 = document.getElementById('but3').addEventListener('click', () => {
+    bandTitles.innerText='band 3'
+})
+
+const but4 = document.getElementById('but4').addEventListener('click', () => {
+    bandTitles.innerText='band 4'
+})
+
+const but5 = document.getElementById('but5').addEventListener('click', () => {
+    bandTitles.innerText='band 5'
+})
+
+const but6 = document.getElementById('but6').addEventListener('click', () => {
+    bandTitles.innerText='band 6'
+})
