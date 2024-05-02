fc6cd7380c5a87078746be437c8610df98b0a25e
Andy <hinchliffe106@gmail.com>
2024-05-02 10:26:12 +0100

    desktop info

--- app_desktop.js
+++ app_desktop.js
@@ -3,123 +3,192 @@ const bandImage = document.getElementById('bandImage')
 
 
 const but1 = document.getElementById('but1').addEventListener('click', () => {
-    bandTitles.innerText='Waterbear College'
-    bandImage.src ="https://images.pexels.com/photos/1327426/pexels-photo-1327426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    bandTitles.innerText='Kali (WaterBear)'
+    bandImage.src ="image1.jpg"
+    bioText.innerText="With a focus on innovation and creativity, WaterBear empowers the next gen musicians and industry creatives to excel across various disciplines, preparing them for success in today's music industry."
+    
 })
 
 const but2 = document.getElementById('but2').addEventListener('click', () => {
+    bandTitles.innerText='Kelci Leigh (WaterBear)'
+    bandImage.src ="image2.jpg"
+    bioText.innerText="With a focus on innovation and creativity, WaterBear empowers the next gen musicians and industry creatives to excel across various disciplines, preparing them for success in today's music industry."
+})
+
+const but2_5 = document.getElementById('but2_5').addEventListener('click', () => {
     bandTitles.innerText='Sheffield College'
+    bandImage.src ="placeholder.jpg"
 })
 
 const but3 = document.getElementById('but3').addEventListener('click', () => {
     bandTitles.innerText='Darlin'
+    bandImage.src ="image4.jpg"
+    bioText.innerText="If you are spending your sweet time off work watching us, we're gonna make sure you enjoy yourself!"
 })
 
 const but4 = document.getElementById('but4').addEventListener('click', () => {
     bandTitles.innerText='Creative DJ School'
+    bandImage.src ="image5.jpg"
+    bioText.innerText='Creative DJ school opened in 2019, offering DJ and Music ProductionTu for schools, charities, fundraising and the general public.'
 })
 
 const but5 = document.getElementById('but5').addEventListener('click', () => {
-    bandTitles.innerText='Lunarca'
+    bandTitles.innerText='The Unbroken'
+    bioText.innerText=''
 })
 
 const but6 = document.getElementById('but6').addEventListener('click', () => {
     bandTitles.innerText='Creative DJ School'
+    bandImage.src ="image5.jpg"
+    bioText.innerText='Creative DJ school opened in 2019, offering DJ and Music ProductionTu for schools, charities, fundraising and the general public.'
 })
 
 const but7 = document.getElementById('but7').addEventListener('click', () => {
-    bandTitles.innerText='Sheffield College'
+    bandTitles.innerText='Faith And Star'
+    bandImage.src ="image7.jpg"
+    bioText.innerText='Faith and Star are Sheffield based singers drawing influence for their love of RnB, Gospel and hip hop.'
 })
 
 const but8 = document.getElementById('but8').addEventListener('click', () => {
     bandTitles.innerText='Jodie Rose'
+    bandImage.src ="image8.jpg"
+    bioText.innerText='Jodie Rose is an artist that plays alt rock, indie, pop, country and more on acoustic guitar so there is something for everyone!'
 })
 
 const but9 = document.getElementById('but9').addEventListener('click', () => {
     bandTitles.innerText='Glass Rhino'
+    bandImage.src ="image9.jpg"
+    bioText.innerText="Glass Rhino are a 4 piece rock band from Sheffield that play originals and covers from the likes of guns'n'roses to pearl jam to Stevie wonder. "
 })
 
 const but10 = document.getElementById('but10').addEventListener('click', () => {
     bandTitles.innerText='Geoff T'
+    bandImage.src ="image10.jpg"
+    bioText.innerText='Geoff Ticehurst has spun alongside worldwide talent and played every genre you can think of at some point or another.'
 })
 
 const but11 = document.getElementById('but11').addEventListener('click', () => {
-    bandTitles.innerText='Waterbear College'
+    bandTitles.innerText='Mae Loughlin'
+    bandImage.src ="image11.jpg"
+    bioText.innerText="Hi, I'm Mae Loughlin, a non-binary, 19-year-old Indie/Folk musician originally from Lincoln."    
 })
 
 const but12 = document.getElementById('but12').addEventListener('click', () => {
     bandTitles.innerText='Creative DJ School'
+    bandImage.src ="image5.jpg"
+    bioText.innerText='Creative DJ school opened in 2019, offering DJ and Music ProductionTu for schools, charities, fundraising and the general public.'
 })
 
 const but13 = document.getElementById('but13').addEventListener('click', () => {
-    bandTitles.innerText='Battle of the bands'
+    bandTitles.innerText='Rock School Band'
+    bandImage.src ="image13.jpg"
+    bioText.innerText="The Rockskool is a local school for under 18’s teaches everything that a normal school or college may miss."
 })
 
 const but14 = document.getElementById('but14').addEventListener('click', () => {
-    bandTitles.innerText='Rock School'
+    bandTitles.innerText='Lexi'
+    bandImage.src ="placeholder.jpg"
+    bioText.innerText=''
 })
 
 const but15 = document.getElementById('but15').addEventListener('click', () => {
     bandTitles.innerText='Alfie Darlin'
+    bandImage.src ="image15.jpg"
+    bioText.innerText="From busking “The Moors” of Sheffield to Half-time center pitch at the Proact Stadium and main stage performances at Chesterfield, Manchester, and Sheffield Pride."
 })
 
 const but16 = document.getElementById('but16').addEventListener('click', () => {
+    bandTitles.innerText='Scarlet Kirwan'
+    bandImage.src ="image16.jpg"
+    bioText.innerText="17 year old singer-songwriter, Scarlett Kirwan has covered some amazing songs adapting them to suit her own unique style of singing such as Joni Mitchell, Eva Cassidy, Karen Carpenter, Taylor Swift, the list goes on."
+
+})
+
+const but16_5 = document.getElementById('but16_5').addEventListener('click', () => {
     bandTitles.innerText='Creative DJ School'
+    bandImage.src ="image5.jpg"
+    bioText.innerText='Creative DJ school opened in 2019, offering DJ and Music ProductionTu for schools, charities, fundraising and the general public.'
 })
 
 const but17 = document.getElementById('but17').addEventListener('click', () => {
     bandTitles.innerText='Jube'
+    bandImage.src ="image17.jpg"
+    bioText.innerText="Sheffield based duo JUBE released their debut album ‘As One Door Closes...' on the Freshly Squeezed label in the summer of 2020. "
+
 })
 
 const but18 = document.getElementById('but18').addEventListener('click', () => {
     bandTitles.innerText='Creative DJ School'
+    bandImage.src ="image5.jpg"
+})
+
+const but18_5 = document.getElementById('but18_5').addEventListener('click', () => {
+    bandTitles.innerText='Georgia Mills'
+    bandImage.src ="image18.jpg"
+    bioText.innerText="Georgie recently opened up BBC Radio 1’s Big Weekend singing just before Ed Sheeran in front of 30-40,000 people."
 })
 
 const but19 = document.getElementById('but19').addEventListener('click', () => {
-    bandTitles.innerText='Georgia Mills'
+    bandTitles.innerText='Matic Mouth'
+    bandImage.src ="image19.jpg"
+    bioText.innerText="Hailing from Sheffield U.K., Matic Mouth is an MC/Rapper who was recently part of Hip-Hop collective Clubs & Spades."
 })
 
 const but20 = document.getElementById('but20').addEventListener('click', () => {
-    bandTitles.innerText='Matic Mouth'
+    bandTitles.innerText='Marcus William III'
+    bandImage.src ="image20.jpg"
+    bioText.innerText="Having supported David Gray, The Stranglers, Terrorvision and Glenn Tillbrook (squeeze) touring Europe and the UK in various bands over the years bringing his own brand of heartfelt upbeat original songs with a twist."
 })
 
 const but21 = document.getElementById('but21').addEventListener('click', () => {
-    bandTitles.innerText='Battle of the bands'
-})
-
-const but22 = document.getElementById('but22').addEventListener('click', () => {
     bandTitles.innerText='Andy H'
+    bandImage.src ="image21.jpg"
+    bioText.innerText="Having supported thousands of World class DJs and artists including Public Enemy, Chase & Status, De La Soul, Disclosure, DJ EZ, My Nu Leng, Major Lazer, Mark Ronson, Andy C, Jazzy Jeff, Annie Mac, Flying Lotus, Pendulum, Bonobo and countless others over the years, Andy has earned himself a reputation as a ‘DJs DJ’. "
 })
 
 // const but23 = document.getElementById('but23').addEventListener('click', () => {
 //     bandTitles.innerText='Andy H'
 // })
 
-const but24 = document.getElementById('but24').addEventListener('click', () => {
+const but22 = document.getElementById('but22').addEventListener('click', () => {
     bandTitles.innerText='Walkley Community Choir'
+    bandImage.src ="image22.jpg"
+    bioText.innerText="Walkley Community Choir is open for everyone who loves singing! We meet on Tuesdays at 6:30 in St Mary's, Howard Road, Walkley."
+})
+
+const but23 = document.getElementById('but23').addEventListener('click', () => {
+    bandTitles.innerText='Penny'
+    bandImage.src ="image23.jpg"
+    bioText.innerText="Penny is a self-produced and self-taught singer / songwriter with a catalogue of meticulously crafted songs."
+})
+
+const but24 = document.getElementById('but24').addEventListener('click', () => {
+    bandTitles.innerText='Rogue'
+    bandImage.src ="placeholder.jpg"
+    bioText.innerText=""
 })
 
 const but25 = document.getElementById('but25').addEventListener('click', () => {
-    bandTitles.innerText='Penny'
+    bandTitles.innerText='April Es'
+    bandImage.src ="image25.jpg"
+    bioText.innerText="With over 50 thousand monthly listeners on Spotify, 23 year old multi-genre vocalist, DJ and busker, April-Ess is one to watch."
 })
 
 const but26 = document.getElementById('but26').addEventListener('click', () => {
-    bandTitles.innerText='Battle of the bands'
+    bandTitles.innerText='Maddie Flemming'
+    bandImage.src ="image26.jpg"
+    bioText.innerText="Professional Female Vocalist and vocal coach from chesterfield. Winner of search for a star and Peak FM vocal Competitions."
 })
 
 const but27 = document.getElementById('but27').addEventListener('click', () => {
-    bandTitles.innerText='April Es'
+    bandTitles.innerText='Shabba'
+    bandImage.src ="image27.jpg"
+    bioText.innerText="Shabbah are a 4 piece indie/ alt rock local band from Sheffield with an original tasting sound, that’s sure to have something for everyone."
 })
 
 const but28 = document.getElementById('but28').addEventListener('click', () => {
-    bandTitles.innerText='Maddie Flemming'
-})
-
-const but29 = document.getElementById('but29').addEventListener('click', () => {
-    bandTitles.innerText='Shabba'
-})
-
-const but30 = document.getElementById('but30').addEventListener('click', () => {
     bandTitles.innerText='Kyla C'
+    bandImage.src ="image28.jpg"
+    bioText.innerText="Kyla C is a regular on the dance, urban & soul nightclub scene sharing line-ups with many of the UK's biggest DJ's & artists."
 })
 
