ee7302ee9f9139d0e63cb654a2919922e8a862e6
Andy <hinchliffe106@gmail.com>
2024-05-28 14:14:33 +0100

    images folder

--- app_desktop.js
+++ app_desktop.js
@@ -3,33 +3,33 @@ const bandImage = document.getElementById('bandImage')
 
 
 const but1 = document.getElementById('but1').addEventListener('click', () => {
-    bandTitles.innerText='James Brown (Waterbear Collage)'
-    bandImage.src ="image1.jpg"
+    bandTitles.innerText='James Henry (Waterbear Collage)'
+    bandImage.src ="images/image1.jpg"
     bioText.innerText="I'm a singer-songwriter based in Sheffield, crafting and performing songs reminiscent of legends like Johnny Cash, Nick Cave, and Tom Petty. My music blends storytelling with rich sound that is both timeless and uniquely my own. My songs aim to connect deeply with listeners through powerful lyrics and compelling performances."
     
 })
 
 const but2 = document.getElementById('but2').addEventListener('click', () => {
     bandTitles.innerText='Kelci Leigh (Sheffield College)'
-    bandImage.src ="image2.jpg"
+    bandImage.src ="images/image2.jpg"
     bioText.innerText="With a focus on innovation and creativity, WaterBear empowers the next gen musicians and industry creatives to excel across various disciplines, preparing them for success in today's music industry."
 })
 
 const but2_5 = document.getElementById('but2_5').addEventListener('click', () => {
     bandTitles.innerText='Rogue Division (Sheffield College)'
-    bandImage.src ="r_division.jpg"
+    bandImage.src ="images/r_division.jpg"
     bioText.innerText="2022 was the year Sheffield Hard Rock outfit Rogue Division first came onto the scene. Featuring some of Sheffield’s finest musicians such as Ryley Donston (Lead Guitar), Ben Grayson (Drums), Theo Ferreyra (Bass), James Bickerton (Rhythm Guitar) and Will Thompson (Vocals) they wowed audiences all around Sheffield with their High Attitude approach to Hard Rock and Punk."
 })
 
 const but3 = document.getElementById('but3').addEventListener('click', () => {
     bandTitles.innerText='Darlin'
-    bandImage.src ="image4.jpg"
+    bandImage.src ="images/image4.jpg"
     bioText.innerText="If you are spending your sweet time off work watching us, we're gonna make sure you enjoy yourself!"
 })
 
 const but4 = document.getElementById('but4').addEventListener('click', () => {
     bandTitles.innerText='Romeo (Creative DJ School)'
-    bandImage.src ="image5.jpg"
+    bandImage.src ="images/image5.jpg"
     bioText.innerText='A year 10 student from Hope Valley College.. He has been DJing for twelve months and already performed twice at his school.Creative DJ school opened in 2019, offering DJ and Music ProductionTu for schools, charities, fundraising and the general public.'
 })
 
@@ -37,42 +37,42 @@ const but5 = document.getElementById('but5').addEventListener('click', () => {
     bandTitles.innerText='The Unbroken (Talent Scout Night)'
     bioText.innerText=`70s rock classics performed true to style, with a couple of modern(ish) ones thrown in for good measure! Formed in 2023,
     found in 1973 - fun guaranteed, flares optional.`
-    bandImage.src ="unbroken.jpg"
+    bandImage.src ="images/unbroken.jpg"
 })
 
 const but6 = document.getElementById('but6').addEventListener('click', () => {
     bandTitles.innerText='Elijah and Thevindhu (Creative DJ School)'
-    bandImage.src ="image5.jpg"
+    bandImage.src ="images/image5.jpg"
     bioText.innerText='A year 8 student from Hope Valley College. Ellijah has been DJing for twelve months and already performed three sets, including a school music showcase in Birmingham. A year 8 student from Hope Valley College. Thevindhu started paired lessons with Ellijah around eight months ago. The Sheffield Food festival will be his first performance to an audience.'
 })
 
 const but7 = document.getElementById('but7').addEventListener('click', () => {
     bandTitles.innerText='Faith And Star (Sheffield Music School)'
-    bandImage.src ="image7.jpg"
+    bandImage.src ="images/image7.jpg"
     bioText.innerText='Faith and Star are Sheffield based singers drawing influence for their love of RnB, Gospel and hip hop.'
 })
 
 const but8 = document.getElementById('but8').addEventListener('click', () => {
     bandTitles.innerText='Jodie Rose'
-    bandImage.src ="image8.jpg"
+    bandImage.src ="images/image8.jpg"
     bioText.innerText='Jodie Rose is an artist that plays alt rock, indie, pop, country and more on acoustic guitar so there is something for everyone!'
 })
 
 const but9 = document.getElementById('but9').addEventListener('click', () => {
     bandTitles.innerText='Glass Rhino'
-    bandImage.src ="image9.jpg"
+    bandImage.src ="images/image9.jpg"
     bioText.innerText="Glass Rhino are a 4 piece rock band from Sheffield that play originals and covers from the likes of guns'n'roses to pearl jam to Stevie wonder. "
 })
 
 const but10 = document.getElementById('but10').addEventListener('click', () => {
     bandTitles.innerText='Geoff T'
-    bandImage.src ="image10.jpg"
+    bandImage.src ="images/image10.jpg"
     bioText.innerText='Geoff Ticehurst has spun alongside worldwide talent and played every genre you can think of at some point or another. (DJing between acts throughout the day)'
 })
 
 const but11 = document.getElementById('but11').addEventListener('click', () => {
     bandTitles.innerText='Mae Loughlin (Waterbear Collage)'
-    bandImage.src ="image11.jpg"
+    bandImage.src ="images/image11.jpg"
     bioText.innerText="Hi, I'm Mae Loughlin, a non-binary, 19-year-old Indie/Folk musician originally from Lincoln."    
 })
 
@@ -84,69 +84,69 @@ const but11 = document.getElementById('but11').addEventListener('click', () => {
 
 const but13 = document.getElementById('but13').addEventListener('click', () => {
     bandTitles.innerText='Rock School Band'
-    bandImage.src ="image13.jpg"
+    bandImage.src ="images/image13.jpg"
     bioText.innerText="The Rockskool is a local school for under 18’s teaches everything that a normal school or college may miss."
 })
 
 const but14 = document.getElementById('but14').addEventListener('click', () => {
     bandTitles.innerText='Lexi'
-    bandImage.src ="lexi.jpg"
+    bandImage.src ="images/lexi.jpg"
     bioText.innerText='Lexi is fresh from her Australian tour and is now back in her hometown ready to bring you some toe tapping anthems. Lexi is a singer-songwriter that will have youall singing along to her indie pop tunes.'
 })
 
 const but15 = document.getElementById('but15').addEventListener('click', () => {
     bandTitles.innerText='Alfie Darlin'
-    bandImage.src ="image15.jpg"
+    bandImage.src ="images/image15.jpg"
     bioText.innerText="From busking “The Moors” of Sheffield to Half-time center pitch at the Proact Stadium and main stage performances at Chesterfield, Manchester, and Sheffield Pride."
 })
 
 const but16 = document.getElementById('but16').addEventListener('click', () => {
     bandTitles.innerText='Scarlet Kirwan (Talent Scout Night)'
-    bandImage.src ="image16.jpg"
+    bandImage.src ="images/image16.jpg"
     bioText.innerText="17 year old singer-songwriter, Scarlett Kirwan has covered some amazing songs adapting them to suit her own unique style of singing such as Joni Mitchell, Eva Cassidy, Karen Carpenter, Taylor Swift, the list goes on."
 
 })
 
 const but16_5 = document.getElementById('but16_5').addEventListener('click', () => {
     bandTitles.innerText='Zach (Creative DJ School)'
-    bandImage.src ="image5.jpg"
+    bandImage.src ="images/image5.jpg"
     bioText.innerText='A year 7 student from Hope Valley College. Zach started DJ lessons 6 months ago and has never performed to an audience.'
 })
 
 const but17 = document.getElementById('but17').addEventListener('click', () => {
     bandTitles.innerText='Jube'
-    bandImage.src ="image17.jpg"
+    bandImage.src ="images/image17.jpg"
     bioText.innerText="Sheffield based duo JUBE released their debut album ‘As One Door Closes...' on the Freshly Squeezed label in the summer of 2020. "
 
 })
 
 const but18 = document.getElementById('but18').addEventListener('click', () => {
     bandTitles.innerText='Elijah and Thevindhu (Creative DJ School)'
-    bandImage.src ="image5.jpg"
+    bandImage.src ="images/image5.jpg"
     bioText.innerText='A year 8 student from Hope Valley College.. Ellijah has been DJing for twelve months and already performed three sets, including a school music showcase in Birmingham. A year 8 student from Hope Valley College. Thevindhu started paired lessons with Ellijah around eight months ago. The Sheffield Food festival will be his first performance to an audience.'
 })
 
 const but18_5 = document.getElementById('but18_5').addEventListener('click', () => {
     bandTitles.innerText='Georgia Mills'
-    bandImage.src ="image18.jpg"
+    bandImage.src ="images/image18.jpg"
     bioText.innerText="Georgie recently opened up BBC Radio 1’s Big Weekend singing just before Ed Sheeran in front of 30-40,000 people."
 })
 
 const but19 = document.getElementById('but19').addEventListener('click', () => {
     bandTitles.innerText='Matic Mouth'
-    bandImage.src ="image19.jpg"
+    bandImage.src ="images/image19.jpg"
     bioText.innerText="Hailing from Sheffield U.K., Matic Mouth is an MC/Rapper who was recently part of Hip-Hop collective Clubs & Spades."
 })
 
 const but20 = document.getElementById('but20').addEventListener('click', () => {
     bandTitles.innerText='Marcus William III (Talent Scout Night)'
-    bandImage.src ="image20.jpg"
+    bandImage.src ="images/image20.jpg"
     bioText.innerText="Having supported David Gray, The Stranglers, Terrorvision and Glenn Tillbrook (squeeze) touring Europe and the UK in various bands over the years bringing his own brand of heartfelt upbeat original songs with a twist."
 })
 
 const but21 = document.getElementById('but21').addEventListener('click', () => {
     bandTitles.innerText='Andy H'
-    bandImage.src ="image21.jpg"
+    bandImage.src ="images/image21.jpg"
     bioText.innerText="Having supported thousands of World class DJs and artists including Public Enemy, Chase & Status, De La Soul, Disclosure, DJ EZ, My Nu Leng, Major Lazer, Mark Ronson, Andy C, Jazzy Jeff, Annie Mac, Flying Lotus, Pendulum, Bonobo and countless others over the years, Andy has earned himself a reputation as a ‘DJs DJ’. (DJing between acts throughout the day) "
 })
 
@@ -156,55 +156,55 @@ const but21 = document.getElementById('but21').addEventListener('click', () => {
 
 const but22 = document.getElementById('but22').addEventListener('click', () => {
     bandTitles.innerText='Walkley Community Choir'
-    bandImage.src ="image22.jpg"
+    bandImage.src ="images/image22.jpg"
     bioText.innerText="Walkley Community Choir is open for everyone who loves singing! We meet on Tuesdays at 6:30 in St Mary's, Howard Road, Walkley."
 })
 
 const but23 = document.getElementById('but23').addEventListener('click', () => {
     bandTitles.innerText='Tara And Chris Marshall-Unitt'
-    bandImage.src ="image23.jpg"
+    bandImage.src ="images/image23.jpg"
     bioText.innerText="Tara and Chris Marshall-Unitt are a dynamic musical duo, both on stage and in life. Married and musically intertwined, they bring a unique blend of rock, folk, and theatrical flair to their performances. Their journey as a musical duo has seen them perform at numerous fundraisers and open mics with Butter Side Up Theatre Company, where their synergy and love for music shine through."
 })
 
 const but24 = document.getElementById('but24').addEventListener('click', () => {
     bandTitles.innerText='Rogue'
-    bandImage.src ="image24.jpg"
+    bandImage.src ="images/image24.jpg"
     bioText.innerText="Featuring the delightful, powerful vocals of Hannah Lily, a four-piece band with the very talented Chris Firminger on guitar, Jon Wynn on Bass and Tom Bryson on Drums. Performing an eclectic mix of songs, full of Sheffield Soul and grit."
 })
 
 const but25 = document.getElementById('but25').addEventListener('click', () => {
     bandTitles.innerText='April Es'
-    bandImage.src ="image25.jpg"
+    bandImage.src ="images/image25.jpg"
     bioText.innerText="With over 50 thousand monthly listeners on Spotify, 23 year old multi-genre vocalist, DJ and busker, April-Ess is one to watch."
 })
 
 const but26 = document.getElementById('but26').addEventListener('click', () => {
     bandTitles.innerText='Maddie Flemming'
-    bandImage.src ="image26.jpg"
+    bandImage.src ="images/image26.jpg"
     bioText.innerText="Professional Female Vocalist and vocal coach from chesterfield. Winner of search for a star and Peak FM vocal Competitions."
 })
 
 const but27 = document.getElementById('but27').addEventListener('click', () => {
     bandTitles.innerText='Shabba'
-    bandImage.src ="image27.jpg"
+    bandImage.src ="images/image27.jpg"
     bioText.innerText="Shabbah are a 4 piece indie/ alt rock local band from Sheffield with an original tasting sound, that’s sure to have something for everyone."
 })
 
 const but28 = document.getElementById('but28').addEventListener('click', () => {
     bandTitles.innerText='Kavita'
-    bandImage.src ="image28.jpg"
+    bandImage.src ="images/image28.jpg"
     bioText.innerText="Kavita is an Award Winning Lawyer/ DJ. Having played at some of the most iconic venues in the UK and abroad. Kavita continues to take her unique story to the road sharing her mix bag of tracks of funky, classic, disco, house music and entertaining music lovers wherever her journey takes her. (DJing between acts throughout the day)"
 })
 
 const but30 = document.getElementById('but30').addEventListener('click', () => {
     bandTitles.innerText='Yorkshire Tea Guy'
-    bandImage.src ="yorkshireteaguy640.jpg"
+    bandImage.src ="images/yorkshireteaguy640.jpg"
     bioText.innerText="“I’m a lad from Yorkshire and I love my Yorkshire Tea!” It all started one Christmas morning with a 3 stringed guitar made by Spatchcock & Wurzil from a dining chair leg and a vintage Yorkshire Tea caddy. The best Christmas present a tea loving musician could hope for! I call it the ‘Teatar!’ A video clip of me playing it was sent to Yorkshire Tea who invited me up to Taylors of Harrogate. I ended up doing a gig in their offices and they invited me to be in their next TV Advert ‘Brewtopia’. That’s where the jacket came from... on set! They let me keep it. The rest as they say is history!"
 })
 
 const but31 = document.getElementById('but31').addEventListener('click', () => {
     bandTitles.innerText='Bandanarama'
-    bandImage.src ="bandanarama640.jpg"
+    bandImage.src ="images/bandanarama640.jpg"
     bioText.innerText="Bandanarama bring a high energy mash up of disco, indie and rock hits with a New Orleans vibe specialising in parades and festival performances. Featuring a line-up of: Trumpets, Saxophones, Trombones, Piccolo, Flutes, Sousaphone and Drums. We have been preforming in parades and festivals since 2015, Audience participation is unavoidable the with well-known tunes getting everyone singing and dancing. We look like a group of rockers/bikers who are not just ‘Born to be Wild’ we are ‘Born to be Wild Fun!"
 })
 
--- app_sun.js
+++ app_sun.js
@@ -6,84 +6,84 @@ const bioText = document.getElementById('bioText')
 
 const but11 = document.getElementById('but11').addEventListener('click', () => {
     bandTitles.innerText='Mae Loughlin (Waterbear Collage)'
-    bandImage.src ="image11.jpg"
+    bandImage.src ="images/image11.jpg"
     bioText.innerText="Hi, I'm Mae Loughlin, a non-binary, 19-year-old Indie/Folk musician originally from Lincoln."
 })
 
 const but12 = document.getElementById('but12').addEventListener('click', () => {
     bandTitles.innerText='Creative DJ School'
-    bandImage.src ="image5.jpg" 
+    bandImage.src ="images/image5.jpg" 
     bioText.innerText="Creative DJ school opened in 2019, offering DJ and Music Production tuition for schools, charities, festivals and the general public."
 })
 
 const but13 = document.getElementById('but13').addEventListener('click', () => {
     bandTitles.innerText='Rock School Band'
-    bandImage.src ="image13.jpg" 
+    bandImage.src ="images/image13.jpg" 
     bioText.innerText="The Rockskool is a local school for under 18’s teaches everything that a normal school or college may miss."
 })
 
 const but14 = document.getElementById('but14').addEventListener('click', () => {
     bandTitles.innerText='Lexi'
-    bandImage.src ="lexi.jpg" 
+    bandImage.src ="images/lexi.jpg" 
     bioText.innerText="Lexi is fresh from her Australian tour and is now back in her hometown ready to bring you some toe tapping anthems. Lexi is a singer-songwriter that will have you all singing along to her indie pop tunes."
 })
 
 const but15 = document.getElementById('but15').addEventListener('click', () => {
     bandTitles.innerText='Alfie Darlin'
-    bandImage.src ="image15.jpg" 
+    bandImage.src ="images/image15.jpg" 
     bioText.innerText="From busking “The Moors” of Sheffield to Half-time center pitch at the Proact Stadium and main stage performances at Chesterfield, Manchester, and Sheffield Pride."
 })
 
 const but6 = document.getElementById('but16').addEventListener('click', () => {
     bandTitles.innerText='Scarlet Kirwan (Talent Scout Night)'
-    bandImage.src ="image16.jpg" 
+    bandImage.src ="images/image16.jpg" 
     bioText.innerText="17 year old singer-songwriter, Scarlett Kirwan has covered some amazing songs adapting them to suit her own unique style of singing such as Joni Mitchell, Eva Cassidy, Karen Carpenter, Taylor Swift, the list goes on."
 })
 
 const but16_5 = document.getElementById('but16_5').addEventListener('click', () => {
     bandTitles.innerText='Zach'
-    bandImage.src ="image5.jpg" 
+    bandImage.src ="images/image5.jpg" 
     bioText.innerText="A year 7 student from Hope Valley College. Zach started DJ lessons 6 months ago and has never performed to an audience."
 })
 
 const but8 = document.getElementById('but17').addEventListener('click', () => {
     bandTitles.innerText='Jube'
-    bandImage.src ="image17.jpg" 
+    bandImage.src ="images/image17.jpg" 
     bioText.innerText="Sheffield based duo JUBE released their debut album ‘As One Door Closes...' on the Freshly Squeezed label in the summer of 2020. "
 })
 
 const but17_5 = document.getElementById('but17_5').addEventListener('click', () => {
     bandTitles.innerText='Elijah and Thevindhu (Creative DJ School)'
-    bandImage.src ="image5.jpg" 
+    bandImage.src ="images/image5.jpg" 
     bioText.innerText="A year 8 studentfrom Hope Valley College. Ellijah has been DJing for twelve months and already performed three sets, including a school music showcase in Birmingham. A year 8 student from Hope Valley College. Thevindhu started paired lessons with Ellijah around eight months ago. The Sheffield Food festival will be his first performance to an audience."
 })
 
 const but18 = document.getElementById('but18').addEventListener('click', () => {
     bandTitles.innerText='Georgia Mills'
-    bandImage.src ="image18.jpg" 
+    bandImage.src ="images/image18.jpg" 
     bioText.innerText="Georgie recently opened up BBC Radio 1’s Big Weekend singing just before Ed Sheeran in front of 30-40,000 people."
 })
 
 const but19 = document.getElementById('but19').addEventListener('click', () => {
     bandTitles.innerText='Matic Mouth'
-    bandImage.src ="image19.jpg" 
+    bandImage.src ="images/image19.jpg" 
     bioText.innerText="Hailing from Sheffield U.K., Matic Mouth is an MC/Rapper who was recently part of Hip-Hop collective Clubs & Spades. "
 })
 
 const but20 = document.getElementById('but20').addEventListener('click', () => {
     bandTitles.innerText='Marcus William III (Talent Scout Night)'
-    bandImage.src ="image20.jpg" 
+    bandImage.src ="images/image20.jpg" 
     bioText.innerText="Having supported David Gray, The Stranglers, Terrorvision and Glenn Tillbrook (squeeze) touring Europe and the UK in various bands over the years bringing his own brand of heartfelt upbeat original songs with a twist."
 })
 
 const but21 = document.getElementById('but21').addEventListener('click', () => {
     bandTitles.innerText='Andy H'
-    bandImage.src ="image21.jpg" 
+    bandImage.src ="images/image21.jpg" 
     bioText.innerText="Having supported thousands of World class DJs and artists including Public Enemy, Chase & Status, De La Soul, Disclosure, DJ EZ, My Nu Leng, Major Lazer, Mark Ronson, Andy C, Jazzy Jeff, Annie Mac, Flying Lotus, Pendulum, Bonobo and countless others over the years, Andy has earned himself a reputation as a ‘DJs DJ’. (DJing between acts throughout the day)"
 })
 
 const but30 = document.getElementById('but30').addEventListener('click', () => {
     bandTitles.innerText='Yorkshire Tea Guy'
-    bandImage.src ="yorkshireteaguy640.jpg" 
+    bandImage.src ="images/yorkshireteaguy640.jpg" 
     bioText.innerText="“I’m a lad from Yorkshire and I love my Yorkshire Tea!” It all started one Christmas morning with a 3 stringed guitar made by Spatchcock & Wurzil from a dining chair leg and a vintage Yorkshire Tea caddy. The best Christmas present a tea loving musician could hope for! I call it the ‘Teatar!’ A video clip of me playing it was sent to Yorkshire Tea who invited me up to Taylors of Harrogate. I ended up doing a gig in their offices and they invited me to be in their next TV Advert ‘Brewtopia’. That’s where the jacket came from... on set! They let me keep it. The rest as they say is history!"
 })
